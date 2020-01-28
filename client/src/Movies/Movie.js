0b58e92d5c3258b01bb7504b82ef20680c2ac31b
Krystal Guzman <kryguzman@gmail.com>
2020-01-28 12:35:09 -0800

    finished stretch with saved list links working

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import MovieCard from './MovieCard'
 
+
 const Movie = (props) => {
   const [movie, setMovie] = useState();
  
   useEffect(() => {
     const id = props.match.params.id;
+    console.log("anything!!!!!!!!", props.match.params.id);
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -19,7 +21,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   const saveMovie = () => {
